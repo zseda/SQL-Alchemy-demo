927b3eaf578f012d243a1300eab3a3c83a24266a
Zeynep Seda Birinci <zseda@gmail.com>
2024-04-22 22:05:56 +0200

    simple and oop approaches

---
+++ oop.py
@@ -0,0 +1,41 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, declarative_base
+from loguru import logger
+
+# create db engine
+db = sa.create_engine("sqlite:///:memory:")
+
+# bind db to a session
+Session = sessionmaker(bind=db)
+
+# Base class to use for actual tables
+Base = declarative_base()
+
+
+# Classes that inherit from Base will be mapped to tables
+class User(Base):
+    __tablename__ = "users"
+    # Mapped is a type hint for the column
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
+    username: Mapped[str]
+    email: Mapped[str]
+
+    # wrapper method developer friendly representation of the object
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, username={self.username}, email={self.email})>"
+
+
+def main() -> None:
+    # create all with base class and metadata schematic
+    Base.metadata.create_all(db)
+    # create instance of the user class
+    user = User(username="zeynep", email="zeynep.birinci@hitec-hamburg.de")
+    # session to interact with the db
+    with Session() as session:
+        session.add(user)
+        session.commit()
+        logger.info((session.query(User).all()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ simple.py
@@ -0,0 +1,45 @@
+import sqlalchemy as sa
+from loguru import logger
+
+# create db engine
+engine = sa.create_engine("sqlite:///:memory:")
+# logging detail for detail debugging
+# engine.echo = True
+# create db connection
+connection = engine.connect()
+# create meta data object
+# allows us to define schema
+metadata = sa.MetaData()
+
+# create actual table
+user_table = sa.Table(
+    "user",
+    metadata,
+    sa.Column("id", sa.Integer, primary_key=True),
+    sa.Column("username", sa.String),
+    sa.Column("email", sa.String),
+)
+
+
+# insert user into table
+def insert_user(username: str, email: str) -> None:
+    query = user_table.insert().values(username=username, email=email)
+    connection.execute(query)
+
+
+def select_user(username: str) -> sa.engine.Result:
+    query = user_table.select().where(user_table.c.username == username)
+    result = connection.execute(query)
+    return result.fetchone()
+
+
+def main() -> None:
+    # create all table
+    metadata.create_all(engine)
+    insert_user("zeynep", "zeynep.birinci@hitec-hamburg.de")
+    logger.info((select_user("zeynep")))
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
